ee469cea8dff5b4ddb1914156bb2a0db8377355f
Ander <ander210194@gmail.com>
2022-02-28 17:32:52 +0100

    añadido base de angular

---
+++ Rminder/web/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmptyComponentComponent } from './components/empty-component/empty-component.component';
+import { HomeComponentComponent } from './pages/home/home-component/home-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmptyComponentComponent,
+    HomeComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
