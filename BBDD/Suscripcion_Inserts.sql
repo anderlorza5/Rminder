371d0350d7afede6893d0ea0a0cbe335321042a1
FrostArrow01 <65225752+FrostArrow01@users.noreply.github.com>
2022-03-12 21:02:30 +0100

    Carpeta BBDD duplicada subida por error

--- BBDD/Suscripcion_Inserts.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Suscripcion (Id_usuario, Nombre, Categoria, Imagen, FechaVencimiento)
-VALUES (1,'Netflix','Entretenimiento','./assets/img/netflix.png','2022-04-20 15:40:45.2756145');
--- BBDD/Usuario_Inserts.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Usuario (Nombre, Contrasenia)
-VALUES ('Paco','1234');
