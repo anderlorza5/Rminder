b8dbeb2dc9216e7db542ef80bd1d763b8f77d97c
FrostArrow01 <65225752+FrostArrow01@users.noreply.github.com>
2022-03-12 21:01:46 +0100

    BBDD con inserts de prueba

---
+++ BBDD/Suscripcion.sql
@@ -0,0 +1,18 @@
+-- Create a new table called '[Suscripcion]' in schema '[dbo]'
+-- Drop the table if it already exists
+IF OBJECT_ID('[dbo].[Suscripcion]', 'U') IS NOT NULL
+DROP TABLE [dbo].[Suscripcion]
+GO
+-- Create the table in the specified schema
+CREATE TABLE [dbo].[Suscripcion]
+(
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, -- Primary Key column
+    [Id_usuario] INT NOT NULL,
+    [Nombre] NVARCHAR(50) NOT NULL,
+    [Categoria] NVARCHAR(50) NOT NULL,
+    [Imagen] NVARCHAR(100) NOT NULL,
+    [FechaVencimiento] DATETIME2 NOT NULL,
+    FOREIGN KEY (Id_usuario) REFERENCES Usuario(Id)
+    -- Specify more columns here
+);
+GO
---
+++ BBDD/Suscripcion_Inserts.sql
@@ -0,0 +1,2 @@
+INSERT INTO Suscripcion (Id_usuario, Nombre, Categoria, Imagen, FechaVencimiento)
+VALUES (1,'Netflix','Entretenimiento','./assets/img/netflix.png','2022-04-20 15:40:45.2756145');
