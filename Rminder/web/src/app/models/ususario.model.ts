5db8c5a4b9388906f37c6739831c592899a20a13
anderlorza5 <98905446+anderlorza5@users.noreply.github.com>
2022-03-13 18:50:58 +0100

    Add files via upload
    añadidos modelos de suscripciones y usuarios

---
+++ Rminder/web/src/app/models/ususario.model.ts
@@ -0,0 +1,18 @@
+export class Usuario {
+  id: number;
+
+  nombre: String;
+
+  contraseña: String;
+
+
+
+
+  constructor() {
+    this.id=0;
+
+    this.nombre="";
+
+    this.contraseña="";
+  }
+}
