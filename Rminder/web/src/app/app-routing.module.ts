c439859c635b7b4e890da97eb849ea37c2880333
Ruiz Supremo <jruizclase@gmail.com>
2022-03-13 18:46:04 +0100

    Plantilla locochona añadida

--- Rminder/web/src/app/app-routing.module.ts
+++ Rminder/web/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GastosComponent } from './components/gastos/gastos.component';
 import { PaginaLoginComponent } from './pages/pagina-login/pagina-login.component';
 
 const routes: Routes = [
-  {path: '', component: PaginaLoginComponent}
+  {path: '', component: PaginaLoginComponent},
+  {path: 'index', component: GastosComponent}
 ];
 
 @NgModule({
--- Rminder/web/src/app/components/gastos/gastos.component.ts
+++ Rminder/web/src/app/components/gastos/gastos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gastos',
   templateUrl: './gastos.component.html',
-  styleUrls: ['./gastos.component.css']
+  styleUrls: ['./gastos.component.css','../../apoyo/css/sb-admin-2.min.css','../../apoyo/vendor/fontawesome-free/css/all.min.css']
 })
 export class GastosComponent implements OnInit {
 
