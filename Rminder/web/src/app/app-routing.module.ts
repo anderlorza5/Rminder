6066280e8a675f33bbf2749bb97e5e5be65944b2
Ander <ander210194@gmail.com>
2022-03-13 17:29:16 +0100

    estructura de componentes

--- Rminder/web/src/app/app-routing.module.ts
+++ Rminder/web/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaLoginComponent } from './paginas/pagina-login/pagina-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PaginaLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Rminder/web/src/app/app.module.ts
+++ Rminder/web/src/app/app.module.ts
@@ -6,11 +6,31 @@ import { AppComponent } from './app.component';
 import { EmptyComponentComponent } from './components/empty-component/empty-component.component';
 import { HomeComponentComponent } from './pages/home/home-component/home-component.component';
 
+import { LoginComponent } from './components/login/login.component';
+import { PaginaLoginComponent } from './paginas/pagina-login/pagina-login.component';
+import { PaginaSuscripcionesComponent } from './paginas/pagina-suscripciones/pagina-suscripciones.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PaginaGastosComponent } from './paginas/pagina-gastos/pagina-gastos.component';
+import { SuscripcionesComponent } from './components/suscripciones/suscripciones.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     EmptyComponentComponent,
-    HomeComponentComponent
+    HomeComponentComponent,
+
+    LoginComponent,
+     PaginaLoginComponent,
+     PaginaSuscripcionesComponent,
+     HeaderComponent,
+     FooterComponent,
+     PaginaGastosComponent,
+     SuscripcionesComponent,
+     GastosComponent,
+
   ],
   imports: [
     BrowserModule,
